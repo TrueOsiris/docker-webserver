62147bbb2441cfe1e4ee8f024f0ab1ee5e4a5a9d
Osiris <tim@chaubet.be>
2017-06-29 13:52:45 +0200

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -f /config/hive.setup.initialised ]; then
+        echo 'initial configuration done.'
+else    
+        ### run once at container start IF no completion file ###
+        
+        # create config subfolder
+        if [ ! -d /config/hive ]; then
+           mkdir -p /config/hive
+        fi
+        
+        chmod -R 775 /config/hive
+        
+        echo -e "Do not remove this file.\nIf you do, root password will be reset to the one in" \
+                "the config file ssh.pass.\n/etc/resolv.conf will be reset to the one in the config" \
+                "file.\n" > /config/hive.setup.initialised
+        date >> /config/hive.setup.initialised
+fi
