509255842fa8122e961a95cca1de71602ce48cdc
Osiris <tim@chaubet.be>
2017-06-29 14:31:45 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,11 +8,15 @@ else
         ### run once at container start IF no completion file ###
         
         # create config subfolder
-        if [ ! -d /config/hive ]; then
-           mkdir -p /config/hive
+        if [ ! -d /config/folder-startup]; then
+           mkdir -p /config/folder-startup
         fi
+        if [ ! -d /config2/folder-startup]; then
+           mkdir -p /config2/folder-startup
+        fi    
         
-        chmod -R 775 /config/hive
+        chmod -R 775 /config/folder-startup
+        chmod -R 775 /config2/folder-startup
         
         echo -e "Do not remove this file.\nIf you do, root password will be reset to the one in" \
                 "the config" \
