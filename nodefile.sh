319af6672cb900052bed50307e450b3863185fbe
Osiris <tim@chaubet.be>
2017-06-30 13:59:25 +0200

    Update nodefile.sh

--- nodefile.sh
+++ nodefile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo nodefile running
-isManager=$(docker node list 2>/dev/null)
+isManager=$(docker node ls --format {{.Hostname}} 2>/dev/null)
 if [ isManager != '' ]
   docker node ls --format {{.Hostname}} > nodes.list
 fi
