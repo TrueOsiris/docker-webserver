2bdb76946c4f12477077939544f1dc6a329ffa2f
Osiris <tim@chaubet.be>
2017-06-30 11:23:03 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,24 +6,24 @@ if [ -f /shared/hive.setup.initialised ]; then
         echo 'initial configuration done.'
 else    
         ### run once at container start IF no completion file ###
+        wget https://apt.dockerproject.org/repo/pool/testing/d/docker-engine/docker-engine_17.05.0~ce~rc3-0~ubuntu-zesty_amd64.deb \
+         -O /tmp/docker-engine.deb
+        dpkg -i /tmp/docker-engine.deb
         
         # create shared subfolder
-        if [ ! -d /shared/docker-install ]; then
-           mkdir -p /shared/docker-install
-           wget https://apt.dockerproject.org/repo/pool/testing/d/docker-engine/docker-engine_17.05.0~ce~rc3-0~ubuntu-zesty_amd64.deb \
-             -O /shared/docker-engine.deb
-           dpkg -i /shared/docker-engine.deb
-        fi
-        if [ ! -d /synced/folderstartup ]; then
-           mkdir -p /synced/folderstartup
-        fi
-        if [ -f /synced/testfile ]; then
-           date >> /synced/testfile 
-        else 
-           echo -e "Testfile created." > /synced/testfile
-        fi
-        cp /tmp/base.conf /shared/
-        cp /tmp/base.conf /synced/
+        #if [ ! -d /shared/docker-install ]; then
+        #   mkdir -p /shared/docker-install
+        #fi
+        #if [ ! -d /synced/folderstartup ]; then
+        #   mkdir -p /synced/folderstartup
+        #fi
+        #if [ -f /synced/testfile ]; then
+        #   date >> /synced/testfile 
+        #else 
+        #   echo -e "Testfile created." > /synced/testfile
+        #fi
+        #cp /tmp/base.conf /shared/
+        #cp /tmp/base.conf /synced/
         
         echo -e "Do not remove this file.\nIf you do, root password will be reset to the one in" \
                 "the config" \
