2551af00faad8a6a41d8dc39255bac4669f6b0a8
Osiris <tim@chaubet.be>
2017-07-04 17:10:00 +0200

    Update workerfile.sh

--- workerfile.sh
+++ workerfile.sh
@@ -4,9 +4,9 @@ function reachnode {
   pingtest=$(ping -w 2 -c 1 $1 2>&1)
   pingtest=$( [[ $pingtest =~ bytes\ from\ ([a-z0-9]*)\.[a-z]*\ \(.* ]] && echo ${BASH_REMATCH[1]} )
   if [ "$pingtest" = "$1" ]; then
-    echo "\"$1\":\"up\"" >> $workerfile
+    echo "\"$1\":\"up\"," >> $workerfile
   else
-    echo "\"$1\":\"down\"" >> $workerfile
+    echo "\"$1\":\"down\"," >> $workerfile
   fi
 }
 function gen_workerfile {
@@ -24,11 +24,12 @@ function gen_workerfile {
       for node in $(<$f)
       do
         if [ "$HOST_HOSTNAME" = "$node" ]; then
-          echo "\"$node\":\"up\"" >> $workerfile
+          echo "\"$node\":\"up\"," >> $workerfile
         else
           reachnode $node
         fi
       done
+      sed -i '$ s/.$//' $workerfile
       echo "}" >> $workerfile
     fi
   done
