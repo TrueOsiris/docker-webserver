273fce006a6a4430fb3922dad73afd749ecd205e
Osiris <tim@chaubet.be>
2017-07-04 11:59:56 +0200

    Update nodefile.sh

--- nodefile.sh
+++ nodefile.sh
@@ -2,6 +2,6 @@
 echo nodefile running
 isManager=$(docker node ls --format {{.Hostname}} 2>/dev/null)
 if [ "$isManager" != "" ]; then
-  docker node ls --format {{.Hostname}} > /synced/nodes.list
+  docker node ls --format {{.Hostname}} > /synced/$(echo $HOST_HOSTNAME).nodes.list
 fi
 
