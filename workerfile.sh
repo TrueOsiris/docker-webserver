b93333d12eb1c38bebd4a11495c11fa7f2287de6
Osiris <tim@chaubet.be>
2017-07-04 14:42:28 +0200

    Update workerfile.sh

--- workerfile.sh
+++ workerfile.sh
@@ -3,6 +3,11 @@ workerfile=/synced/workers/$(echo $HOST_HOSTNAME)
 if [ -f $workerfile ]; then
   if [ "$(( $(date +"%s") - $(stat -c "%Y" $workerfile) ))" -gt "5" ]; then
     echo "test" > $workerfile
+    for f in /synced/managers/*.nodes
+    do
+      partofswarm=
+      echo "checking $f" >> $workerfile
+    done
   fi
 else
   echo "test" > $workerfile
