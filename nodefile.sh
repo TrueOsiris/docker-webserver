4f86121153e0caf53295fb02b542428e82c5b7c6
Osiris <tim@chaubet.be>
2017-07-04 15:03:59 +0200

    Update nodefile.sh

--- nodefile.sh
+++ nodefile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 isManager=$(docker node ls --format {{.Hostname}} 2>/dev/null)
 if [ "$isManager" != "" ]; then
-  managerfile=/synced/managers/$(echo $HOST_HOSTNAME)
+  managerfile=/synced/managers/$HOST_HOSTNAME
   if [ -f $managerfile ]; then
-    if [ "$(( $(date +"%s") - $(stat -c "%Y" $managerfile ))" -gt "60" ]; then
+    if [ "$(( $(date +"%s") - $(stat -c "%Y" $managerfile )))" -gt "60" ]; then
       docker node ls --format {{.Hostname}} > $managerfile
     fi
   else
