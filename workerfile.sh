3ee467761434f9c6f17093e12fd572a60070ae17
Osiris <tim@chaubet.be>
2017-07-04 14:47:29 +0200

    Update workerfile.sh

--- workerfile.sh
+++ workerfile.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 workerfile=/synced/workers/$(echo $HOST_HOSTNAME)
 if [ -f $workerfile ]; then
-  if [ "$(( $(date +"%s") - $(stat -c "%Y" $workerfile) ))" -gt "5" ]; then
+  if [ "$(( $(date +"%s") - $(stat -c "%Y" $workerfile) ))" -gt "4" ]; then
     echo "test" > $workerfile
-    for f in /synced/managers/*.nodes
+    for f in /synced/managers/*
     do
-      partofswarm=
+      partofswarm=no
       echo "checking $f" >> $workerfile
+      for word in $(<$f)
+      do
+        if [ "$HOST_HOSTNAME" = "$word" ];
+          echo "worker $HOST_HOSTNAME is part of $f" >> $workerfile
+          partofswarm=yes
+        fi
+      done
     done
   fi
 else
