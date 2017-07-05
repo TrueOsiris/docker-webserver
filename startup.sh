7916f1ecfb0655619925a2c190a8d28fa9912dde
Osiris <tim@chaubet.be>
2017-07-05 17:29:39 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -27,18 +27,7 @@ else
         #fi
         #cp /tmp/base.conf /shared/
         #cp /tmp/base.conf /synced/
-
-        #apache2 conf adaptations
-        a2enmod rewrite
-        chown -R www-data:www-data /synced/www /var/log/apache2
-        sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/synced\/www\/' /etc/apache2/sites-enabled/000*.conf
-        #sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php7/apache2/php.ini
-        #sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
-        #sed -i -e "s@\$ALLOW_CUSTOM_SERVERS.*@\$ALLOW_CUSTOM_SERVERS = TRUE;@g" /var/www/mywebsql/config/servers.php
-        rm -R /var/www/html/
-        #to fix error relate to ip address of container apache2
-        echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
-        ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
+        echo "<html><body>test /synced/www/</body></html>" > /synced/www/index.html
 
         echo -e "Do not remove this file.\nIf you do, container will be fully reset on next start." > /synced/$(echo $initfile)
         date >> /synced/$(echo $initfile)
