5f5d0b694fe324036003059f8cd617d72c0f8320
Osiris <tim@chaubet.be>
2017-07-05 16:27:42 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -19,6 +19,17 @@ else
         fi
         if [ ! -d /synced/www ]; then
            mkdir -p /synced/www
+            #apache2 conf
+           a2enmod rewrite
+           chown -R www-data:www-data /synced/www /var/log/apache2
+           sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/synced\/www\/' /etc/apache2/sites-enabled/000*.conf
+           #sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php7/apache2/php.ini
+           #sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php5/apache2/php.ini
+           #sed -i -e "s@\$ALLOW_CUSTOM_SERVERS.*@\$ALLOW_CUSTOM_SERVERS = TRUE;@g" /var/www/mywebsql/config/servers.php
+           rm -R /var/www/html/
+           #to fix error relate to ip address of container apache2
+           echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
+           ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
         fi
         #if [ -f /synced/testfile ]; then
         #   date >> /synced/testfile 
