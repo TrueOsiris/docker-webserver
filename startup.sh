df0706e8f048bf57600c9f4d4092ef7c4bcf3a6e
Osiris <tim@chaubet.be>
2017-07-07 15:49:18 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,11 +5,10 @@ initfile=$(echo $HOST_HOSTNAME)\.initialised
 if [ -f /synced/$(echo $initfile) ]; then
         echo 'initial configuration done.'
 else    
-        ### run once at container start IF no initialization file ###
-        #wget https://apt.dockerproject.org/repo/pool/testing/d/docker-engine/docker-engine_17.05.0~ce~rc3-0~ubuntu-zesty_amd64.deb \
-        # -O /tmp/docker-engine.deb
-        #dpkg -i /tmp/docker-engine.deb
-        
+        ### run once at container start IF no initialization file.
+        ### if the .initialised file is removed, the container    
+        ### will be reset to it's default state, unless the www
+        ### folder is maintained.
         
         if [ ! -d /synced/workers ]; then
            mkdir -p /synced/workers
@@ -19,19 +18,13 @@ else
         fi
         if [ ! -d /synced/www ]; then
            mkdir -p /synced/www
+           git clone git://github.com/fuel/fuel.git /synced/www/
+           cd /synced/www 
+           composer install --prefer-dist
+           composer install --prefer-dist
+           echo "<? header('Location: /public/'); ?>" > /synced/www/index.php
+           
         fi
-        #if [ -f /synced/testfile ]; then
-        #   date >> /synced/testfile 
-        #else 
-        #   echo -e "Testfile created." > /synced/testfile
-        #fi
-        #cp /tmp/base.conf /shared/
-        #cp /tmp/base.conf /synced/
-        git clone git://github.com/fuel/fuel.git /synced/www/
-        cd /synced/www 
-        composer install --prefer-dist
-        composer install --prefer-dist
-        echo "<? header('Location: /public/'); ?>" > /synced/www/index.php
 
         echo -e "Do not remove this file.\nIf you do, container will be fully reset on next start." > /synced/$(echo $initfile)
         date >> /synced/$(echo $initfile)
