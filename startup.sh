8c365c543311090c759a2785f0d9e054bfd97076
Osiris <tim@chaubet.be>
2018-02-22 16:23:33 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,8 @@ else
         ### if the .initialised file is removed, the container    
         ### will be reset to it's default state, unless the www
         ### folder is maintained.
+        mv /etc/php/7.0/apache2/php.ini /config/php.ini
+        ln -s /config/php.ini /etc/php/7.0/apache2/php.ini
         
         if [ ! -f /www/index.php ]; then
            echo "<? header('Location: /test.php'); ?>" > /www/index.php
