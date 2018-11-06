cabdd0d7dad9f737fa3619cad1067da6855adda7
Osiris <tim@chaubet.be>
2018-11-06 11:18:48 +0100

    Update webserver.startup.sh

--- webserver.startup.sh
+++ webserver.startup.sh
@@ -5,10 +5,10 @@ initfile=webserver.initialised
 
 if [ -f /config/php.ini ]; then
 	TZ=${TZ:-"Europe/Brussels"}
-	sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g" /etc/php/7.0/apache2/php.ini
-        mv /etc/php/7.0/apache2/php.ini /config/php.ini
+	sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g" /etc/php/7.0/apache2/php.ini 2>&1
+        mv /etc/php/7.0/apache2/php.ini /config/php.ini 2>&1
         rm /etc/php/7.0/apache2/php.ini 2>&1
-        ln -s /config/php.ini /etc/php/7.0/apache2/php.ini
+        ln -s /config/php.ini /etc/php/7.0/apache2/php.ini 2>&1
         # Enabling PHP mod rewrite
         /usr/sbin/a2enmod rewrite 2>&1
 fi
