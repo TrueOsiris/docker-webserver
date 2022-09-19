e048d022ab93cd572124103760e0518db7f03c3b
trueosiris <tim@chaubet.be>
2022-09-19 16:10:22 +0200

    testing latest 8.2 rc2

--- start.sh
+++ start.sh
@@ -3,21 +3,24 @@
 set -e
 initfile=webserver.initialised
 
-if [ -f /config/php.ini ]; then
+if [ ! -f /config/php.ini ]; then
 	echo "Setting timezone TZ to default Europe/Brussels or to the docker parameter TZ"
 	TZ=${TZ:-"Europe/Brussels"}
-	echo "Replacing timezone in /etc/php/8.2/apache2/php.ini ..."
-	sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g" /etc/php/8.2/apache2/php.ini 2>&1
-	echo "Moving /etc/php/8.2/apache2/php.ini to /config/php.ini ..."
-	mv /etc/php/8.2/apache2/php.ini /config/php.ini 2>&1
-	echo "Creating a symlink to source /config/php.ini at target /etc/php/7.4/apache2/php.ini ..."
+	cp /usr/local/etc/php/php.ini-production /config/php.ini 2>&1
+	echo "Replacing timezone in php.ini ..."
+	sed -i "s#^;date.timezone =.*#date.timezone = ${TZ}#g" /config/php.ini 2>&1
+	#echo "Moving /etc/php/8.2/apache2/php.ini to /config/php.ini ..."
+	#mv /etc/php/8.2/apache2/php.ini /config/php.ini 2>&1
+	echo "Creating a symlink to source /config/php.ini at target /etc/php/8.2/apache2/php.ini ..."
+	mkdir -p /etc/php/8.2/apache2 2>&1
 	ln -s /config/php.ini /etc/php/8.2/apache2/php.ini 2>&1
+	ln -s /config/php.ini /etc/php/php.ini 2>&1
 	echo "Enabling PHP mod rewrite ..."
 	/usr/sbin/a2enmod rewrite 2>&1
 fi
 
 if [ -f /config/$(echo $initfile) ]; then
-        echo 'initial configuration done.'
+        echo 'initial configuration already done. index.php exists.'
 else    
         ### run once at container start IF no initialization file.
         ### if the .initialised file is removed, the container    
